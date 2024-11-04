a49b9cd3c7d1381a559f2117f2b5a8b615dbb23b
Student11 <parashchyk@icloud.com>
2024-11-04 14:48:29 +0200

    ++

--- new1.5/file1.5/BaskFF/static2/app.py
+++ new1.5/file1.5/BaskFF/static2/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 DB_USER = 'SergNik'
 DB_PASSWORD = 'Sergiy2024'
 DB_HOST = 'localhost'
-DB_PORT = '5432'
+DB_PORT = '5433'
 DB_NAME = 'sample30'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
